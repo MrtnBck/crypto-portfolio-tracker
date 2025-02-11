3de1a47f95a0add43bee3f74ee75b07769561ed9
MrtnBck <martin.dev.dev@gmail.com>
2025-02-11 10:23:27 +0100

    coin gecko test request

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
+import axios from "axios";
+
+const coinGeckoBaseUrl = "https://api.coingecko.com/api/v3";
+
+const coinGeckoPing = async () => {
+  const options = {
+    method: "GET",
+    url: coinGeckoBaseUrl + "/ping",
+    params: { x_cg_demo_api_key: import.meta.env.REACT_APP_COIN_GECKO_API_KEY },
+    headers: { accept: "application/json" },
+  };
+
+  try {
+    const response = await axios.request(options);
+    console.log(response.data);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
 function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline">Crypto Project Tracker App</h1>
+      <button onClick={coinGeckoPing}>Ping Coin Gecko</button>
     </>
   );
 }
